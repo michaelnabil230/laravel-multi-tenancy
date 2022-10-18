bdd4794ba520497079192cf4e9421e5e1a553bf7
michaelnabil230 <michaelnabil230@users.noreply.github.com>
2022-10-18 22:58:26 +0000

    Fix styling

--- src/MultiTenancyServiceProvider.php
+++ src/MultiTenancyServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace MichaelNabil230\MultiTenancy;
 
-use MichaelNabil230\MultiTenancy\Commands\MultiTenancyCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
